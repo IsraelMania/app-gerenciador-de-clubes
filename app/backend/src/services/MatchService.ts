2e907d5cf853400f817a18a72864128441b522b7
Irael Mania <israel.abreu6@gmail.com>
2024-06-26 20:28:10 -0300

    fix

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -4,8 +4,8 @@ import TeamService from './TeamService';
 import { MatchType } from '../Types/MatchType';
 
 type DefaultResponse = {
-  num?: number;
-  message?: string;
+    num?: number;
+    message?: string;
   result?: Match;
 };
 
@@ -51,63 +51,54 @@ export default class MatchService {
         },
       ],
     }) as unknown as Promise<MatchType[]>;
-  }
+}
 
-  static async getMatches(inProgress: string | undefined) {
-    if (inProgress === 'true') return this.getMatchesInProgress();
-
-    if (inProgress === 'false') return this.getMatchesNotInProgress();
-
-    return Match.findAll({
-      include: [
-        {
-          model: Team,
-          as: 'homeTeam',
-          attributes: ['teamName'],
-        },
-        {
-          model: Team,
-          as: 'awayTeam',
-          attributes: ['teamName'],
-        },
-      ],
-    });
-  }
-
-  static async finishMatch(matchId: number) {
-    Match.update({ inProgress: false }, { where: { id: matchId } });
-    return { message: 'Finished' };
-  }
-
-  static async updateMatch(matchId: number, homeTeamGoals: number, awayTeamGoals: number) {
-    Match.update(
-      { homeTeamGoals, awayTeamGoals },
-      { where: { id: matchId } },
-    );
-    return { message: 'Updated' };
-  }
-
-  static async createMatch(
-    homeTeamId: number,
-    awayTeamId: number,
-    homeTeamGoals: number,
-    awayTeamGoals: number,
-  ): Promise<DefaultResponse> {
-    if (homeTeamId === awayTeamId) return { num: 422, message: MatchService.equalTeamsMessage };
-
-    const homeTeam = await TeamService.checkTeamExists(homeTeamId);
-    const awayTeam = await TeamService.checkTeamExists(awayTeamId);
-
-    if (!homeTeam || !awayTeam) return { num: 404, message: MatchService.noTeamMessage };
-
-    const response = await Match.create({
-      homeTeamId,
-      awayTeamId,
-      homeTeamGoals,
-      awayTeamGoals,
-      inProgress: true,
-    });
-
-    return { result: response };
-  }
+static async getMatches(inProgress: string | undefined) {
+if (inProgress === 'true') return this.getMatchesInProgress();
+if (inProgress === 'false') return this.getMatchesNotInProgress();
+return Match.findAll({
+include: [
+  {
+    model: Team,
+    as: 'homeTeam',
+    attributes: ['teamName'],
+  },
+  {
+    model: Team,
+    as: 'awayTeam',
+    attributes: ['teamName'],
+  },
+],
+});
+}
+static async finishMatch(matchId: number) {
+Match.update({ inProgress: false }, { where: { id: matchId } });
+return { message: 'Finished' };
+}
+static async updateMatch(matchId: number, homeTeamGoals: number, awayTeamGoals: number) {
+Match.update(
+{ homeTeamGoals, awayTeamGoals },
+{ where: { id: matchId } },
+);
+return { message: 'Updated' };
+}
+static async createMatch(
+homeTeamId: number,
+awayTeamId: number,
+homeTeamGoals: number,
+awayTeamGoals: number,
+): Promise<DefaultResponse> {
+if (homeTeamId === awayTeamId) return { num: 422, message: MatchService.equalTeamsMessage };
+const homeTeam = await TeamService.checkTeamExists(homeTeamId);
+const awayTeam = await TeamService.checkTeamExists(awayTeamId);
+if (!homeTeam || !awayTeam) return { num: 404, message: MatchService.noTeamMessage };
+const response = await Match.create({
+homeTeamId,
+awayTeamId,
+homeTeamGoals,
+awayTeamGoals,
+inProgress: true,
+});
+return { result: response };
+}
 }
