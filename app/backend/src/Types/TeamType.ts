ff3e2378f6426741629dce70831a31fbf23166bf
Irael Mania <israel.abreu6@gmail.com>
2024-06-26 19:56:07 -0300

    #23 lint

--- app/backend/src/Types/TeamType.ts
+++ app/backend/src/Types/TeamType.ts
@@ -1,12 +1,12 @@
 export type TeamType = {
-    name: string;
-    totalPoints: number;
-    totalGames: number;
-    totalVictories: number;
-    totalDraws: number;
-    totalLosses: number;
-    goalsFavor: number;
-    goalsOwn: number;
-    goasBalance?: number;
-    efficiency?: number;
-  };
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goasBalance?: number;
+  efficiency?: number;
+};
