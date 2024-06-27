9a5a41e24f6a0716f63207de560ed8e6e84c226c
Irael Mania <israel.abreu6@gmail.com>
2024-06-26 21:30:24 -0300

    lint

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -1,25 +1,25 @@
 import { Request, Response } from 'express';
-import LeaderbordService from '../services/LeaderbordService';
+import LeaderbordService from 'src/services/LeaderbordService.1';
 
 export default class LeaderboardController {
-    static async getHomeLeaderboard(req: Request, res: Response) {
-        try {
-          const leaderboard = await LeaderbordService.getHomeLeaderboard();
-          return res.status(200).json(leaderboard);
-        } catch (error) {
-          return res.status(500).json({ error: 'Failed' });
-        }
-      }
-    
-      static async getAwayLeaderboard(req: Request, res: Response) {
-        try {
-          const leaderboard = await LeaderbordService.getAwayLeaderboard();
-          return res.status(200).json(leaderboard);
-        } catch (error) {
-          return res.status(500).json({ error: 'Failed' });
-        }
-      }
-      
+  static async getHomeLeaderboard(req: Request, res: Response) {
+    try {
+      const leaderboard = await LeaderbordService.getHomeLeaderboard();
+      return res.status(200).json(leaderboard);
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed' });
+    }
+  }
+
+  static async getAwayLeaderboard(req: Request, res: Response) {
+    try {
+      const leaderboard = await LeaderbordService.getAwayLeaderboard();
+      return res.status(200).json(leaderboard);
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed' });
+    }
+  }
+
   static async getLeaderboard(req: Request, res: Response) {
     try {
       const leaderboard = await LeaderbordService.getLeaderboard();
